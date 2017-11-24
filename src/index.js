4e71cfa4cbc44c7f0e37e17a9b7e513970fa7eca
jacktown11 <jikai_tang@163.com>
2017-11-24 16:40:42 +0800

    loading Data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
@@ -15,6 +16,7 @@ function component() {
 
 	element.appendChild(myIcon);
 
+	console.log(Data);
 
   return element;
 }
