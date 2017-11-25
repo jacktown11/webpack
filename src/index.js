c8cea6c637c25df8fe8d1c75c44e5d85e70c5dda
jacktown11 <jikai_tang@163.com>
2017-11-25 08:15:32 +0800

    clean and prepare for Output Management

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
-  var element = document.createElement('div');
+	var element = document.createElement('div');
 
-  // lodash now is imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+	// lodash now is imported by this script
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-	// Add the image to our existing div.
-	var myIcon = new Image();
-	myIcon.src = Icon;
-
-	element.appendChild(myIcon);
-
-	console.log(Data);
-
-  return element;
+	return element;
 }
 
 document.body.appendChild(component());
