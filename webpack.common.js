dce54058427a62a5c8bb7b63c3bd2184d48e4e1a
jacktown11 <jikai_tang@163.com>
2017-11-25 14:42:56 +0800

    using html-webpack-plugin with template

--- webpack.common.js
+++ webpack.common.js
@@ -17,7 +17,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Production'
+      title: 'Production',
+      template: 'src/index.html'
     })
   ],
   output: {
